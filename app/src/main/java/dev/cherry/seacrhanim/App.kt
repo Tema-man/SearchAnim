7eeba6c51d246f4dcf7198251df09d5fafcd59c2
Artemii Vishnevskii <temaa.mann@gmail.com>
2017-02-13 18:09:13 +0300

    added app class

---
+++ app/src/main/java/dev/cherry/seacrhanim/App.kt
@@ -0,0 +1,14 @@
+package dev.cherry.seacrhanim
+
+import android.app.Application
+
+/**
+ * @author DVLP_2
+ * @since 13.02.2017.
+ */
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
