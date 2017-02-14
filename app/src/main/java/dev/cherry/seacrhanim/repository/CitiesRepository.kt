92fceb3382e3d9222312906ed38f37555ce826da
Artemii Vishnevskii <temaa.mann@gmail.com>
2017-02-14 18:15:09 +0300

    request parsing

--- app/src/main/java/dev/cherry/seacrhanim/repository/CitiesRepository.kt
+++ app/src/main/java/dev/cherry/seacrhanim/repository/CitiesRepository.kt
@@ -1,35 +1,45 @@
 package dev.cherry.seacrhanim.repository
 
+import android.accounts.NetworkErrorException
+import dev.cherry.seacrhanim.App
 import dev.cherry.seacrhanim.entity.City
 import dev.cherry.seacrhanim.net.RestApi
 import dev.cherry.seacrhanim.utils.JsonParser
-import io.reactivex.Observable
 import okhttp3.Request
+import org.json.JSONObject
+import javax.inject.Inject
 
 /**
  * @author DVLP_2
  * *
  * @since 13.02.2017.
  */
+class CitiesRepository {
 
-class CitiesRepository(private val mRestApi: RestApi) {
+    @Inject
+    lateinit var mRestApi: RestApi
 
-    fun getCities(name: String, lang: String): Observable<List<City>> {
-        return Observable.create { emitter ->
-            try {
-                emitter.onNext(requestCities(name, lang))
-                emitter.onComplete()
-            } catch (e: Exception) {
-                emitter.onError(e)
-            }
+    init {
+        App.graph.inject(this)
+    }
+
+    fun getCities(name: String, lang: String): List<City> {
+        try {
+            return requestCities(name, lang)
+        } catch (e: Exception) {
+            throw NetworkErrorException(e)
         }
     }
 
     private fun requestCities(name: String, lang: String): List<City> {
         val request = Request.Builder()
-                .url("https://yasen.hotellook.com/autocomplete?$name=mow&lang=$lang")
+                .url("https://yasen.hotellook.com/autocomplete?term=$name&lang=$lang")
                 .get().build()
 
-        return JsonParser.arrayList(mRestApi.makeRequest(request), City::class.java)
+        val response: String = mRestApi.makeRequest(request)
+        val obj: JSONObject = JSONObject(response)
+        val citiesStr = obj.getJSONArray("cities").toString()
+
+        return JsonParser.arrayList(citiesStr, City::class.java)
     }
 }
