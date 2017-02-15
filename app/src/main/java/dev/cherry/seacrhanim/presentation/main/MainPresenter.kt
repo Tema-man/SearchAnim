3c1edb789690370d2336fc6a7348f2eefb7e442d
Artemii Vishnevskii <temaa.mann@gmail.com>
2017-02-15 18:17:35 +0300

    added points store

--- app/src/main/java/dev/cherry/seacrhanim/presentation/main/MainPresenter.kt
+++ app/src/main/java/dev/cherry/seacrhanim/presentation/main/MainPresenter.kt
@@ -1,10 +1,12 @@
 package dev.cherry.seacrhanim.presentation.main
 
+import android.util.Log
 import com.arellomobile.mvp.InjectViewState
 import com.arellomobile.mvp.MvpPresenter
 import dev.cherry.seacrhanim.App
 import dev.cherry.seacrhanim.entity.City
 import dev.cherry.seacrhanim.repository.CitiesRepository
+import java.util.*
 import javax.inject.Inject
 
 /**
@@ -14,14 +16,38 @@ import javax.inject.Inject
 @InjectViewState
 class MainPresenter : MvpPresenter<MainView>() {
 
+    val TAG: String = "MainPresenter"
+
     @Inject
     lateinit var mCityRepo: CitiesRepository
+
+    lateinit var mSource: City
+    lateinit var mDestination: City
 
     init {
         App.graph.inject(this)
     }
 
     fun getCities(name: String, lang: String): List<City> {
-        return mCityRepo.getCities(name, lang)
+        try {
+            return mCityRepo.getCities(name, lang)
+        } catch (e: Exception) {
+            viewState.showError(e)
+            return Collections.emptyList()
+        }
+    }
+
+    fun sourceSelected(source: City) {
+        mSource = source
+        Log.d(TAG, "source selected: $source")
+    }
+
+    fun destinationSelected(destination: City) {
+        mDestination = destination
+        Log.d(TAG, "destination selected: $destination")
+    }
+
+    fun fabClick() {
+        viewState.navigateToMap(mSource, mDestination)
     }
 }
