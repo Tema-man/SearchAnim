a38e0a2e93e5ec60cae831ab42cb00420457516d
Artemii Vishnevskii <temaa.mann@gmail.com>
2017-03-06 11:56:18 +0300

    Point class moved to geoProjector

--- app/src/main/java/dev/cherry/seacrhanim/presentation/map/utils/GeoProjector.kt
+++ app/src/main/java/dev/cherry/seacrhanim/presentation/map/utils/GeoProjector.kt
@@ -10,45 +10,44 @@ import com.google.android.gms.maps.model.LatLng
  */
 interface GeoProjector {
 
+    /** Data class to store plane coordinates */
+    data class Point(val x: Double, val y: Double)
+
     /**
      * Project [LatLng] to plane
      *
      * @param latLng [LatLng] latLng to project
-     * @return [GeoInterpolator.Point] projected point
+     * @return [Point] projected point
      */
-    fun toPoint(latLng: LatLng): GeoInterpolator.Point
+    fun toPoint(latLng: LatLng): Point
 
     /**
-     * Unproject [GeoInterpolator.Point] from plane back to [LatLng]
+     * Unproject [Point] from plane back to [LatLng]
      *
-     * @param point [GeoInterpolator.Point] point to unproject
+     * @param point [Point] point to unproject
      * @return [LatLng] projected latLng
      */
-    fun toLatLng(point: GeoInterpolator.Point): LatLng
+    fun toLatLng(point: Point): LatLng
 
     /**
      * GeoProjector implementation. Used Mercator projection to project geo coordinates to plane
      */
-    class Mercator() : GeoProjector {
+    class Mercator : GeoProjector {
 
         /** Needs to project geo coordinates to a plane using Mercator projection */
         private val DEFAULT_WORLD_WIDTH = 100.0
 
-        constructor (width: Double) : this() {
-            worldWidth = width
-        }
-
         private var worldWidth = DEFAULT_WORLD_WIDTH
 
-        override fun toPoint(latLng: LatLng): GeoInterpolator.Point {
+        override fun toPoint(latLng: LatLng): Point {
             var x = latLng.longitude / 360.0 + 0.5
             x = if (x > 0.5) 0.5 + (1.0 - x) else (0.5 - x)
             val siny = Math.sin(Math.toRadians(latLng.latitude))
             val y = 0.5 * Math.log((1.0 + siny) / (1.0 - siny)) / (-2.0 * Math.PI) + 0.5
-            return GeoInterpolator.Point(x * worldWidth, y * worldWidth)
+            return Point(x * worldWidth, y * worldWidth)
         }
 
-        override fun toLatLng(point: GeoInterpolator.Point): LatLng {
+        override fun toLatLng(point: Point): LatLng {
             var x = if (point.x > 0.5) (1.5 - point.x) else (0.5 - point.x)
             x = x / worldWidth - 0.5
             val lng = x * 360.0
